befd7fca3035f158386ee12bbbdcbbcb876477d5
Chioma750 <ugwuchioma778@gmail.com>
2026-09-04 18:00:18 +0100

    calculator

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-first_number = float(input("Enter the first number: "))
-operator = input("Choose an operator (+, -, *, /): ")
-second_number = float(input("Enter the second number: "))
-# print(f"{first_number} and {second_number}")
+while True:
+    try:
+        first_number = float(input("Enter the first number: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
 
-if operator == "+":
-    result = first_number + second_number
-    print(f"{first_number} + {second_number} = {result}")    
+    operator = input("Choose an operator (+, -, *, /): ")
 
-elif operator == "-":
-    result = first_number - second_number
-    print(f"{first_number} - {second_number} = {result}")    
+    try:
+        second_number = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
 
-elif operator == "*":
-    result = first_number * second_number
-    print(f"{first_number} * {second_number} = {result}")    
+    if operator == "+":
+        result = first_number + second_number
+        print(f"{first_number} + {second_number} = {result}")    
 
-elif operator == "/":
-    result = first_number / second_number
-    print(f"{first_number} / {second_number} = {result}")    
+    elif operator == "-":
+        result = first_number - second_number
+        print(f"{first_number} - {second_number} = {result}")
 
-else:
-    print("Invalid operator")
+    elif operator == "*":
+        result = first_number * second_number
+        print(f"{first_number} * {second_number} = {result}")    
+
+    elif operator == "/":  
+        try:
+            result = first_number / second_number
+            print(f"{first_number} / {second_number} = {result}") 
+        except ZeroDivisionError:
+            print("You can't divide by zero because, division by zero is underfined.")   
+
+    else:
+        print("Invalid operator")
+
+    continue_calc = input("Do you want to calculate again? (yes/no): ")
+    if continue_calc.lower() == "yes":
+        continue
+    else:
+        break
